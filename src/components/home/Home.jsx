5a2f57e854cc039d476e5325c7c9b7b30cfac69b
kentatixx <gesorokent@gmail.com>
2025-11-13 20:48:36 +0800

    removing comments

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -59,33 +59,7 @@ const Home = () => {
       </div>
     </section>
   );
-  //   <nav className="flex flex-row gap-x-2 w-full z-20 fixed top-0 left-0 h-12 border-b place-content-center items-center bg-gray-200">
-  //     {userLoggedIn ? (
-  //       <>
-  //         <button
-  //           onClick={() => {
-  //             doSignOut().then(() => {
-  //               navigate("/login");
-  //             });
-  //           }}
-  //           className="text-sm text-blue-600 underline"
-  //         >
-  //           Logout
-  //         </button>
-  //       </>
-  //     ) : (
-  //       <>
-  //         <Link className="text-sm text-blue-600 underline" to={"/login"}>
-  //           Login
-  //         </Link>
-  //         <Link className="text-sm text-blue-600 underline" to={"/register"}>
-  //           Register New Account
-  //         </Link>
-  //       </>
-  //     )}
-  //   </nav>
-  // );
+
 };
 
 export default Home;
-// return (
