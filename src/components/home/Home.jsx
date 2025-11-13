64961cf1468f8f7ac524480a0edf3745141b508b
Stayve Alreach Fedilalga <programmerstayve@gmail.com>
2025-11-13 22:46:51 +0800

    login ui update

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,10 +4,65 @@ import { useAuth } from "../../contexts/authContext";
 import { doSignOut } from "../../firebase/auth";
 
 const Home = () => {
-  const { currentUser } = useAuth();
+  const { currentUser, userLoggedIn } = useAuth();
   const navigate = useNavigate();
-  const { userLoggedIn } = useAuth();
-  return <section>Home</section>;
+
+  return (
+    <section className="bigBgImage section h-screen flex items-center justify-center bg-Image p-5">
+      <div className="flex items-center justify-center flex-col gap-5 text-center">
+
+        {/* TEXT SECTION */}
+        <div className="flex flex-col">
+          <h1 className="text-[85px] text-white leading-none">
+            Welcomeee
+          </h1>
+
+          <h3 className="text-[60px] text-white leading-tight mt-2">
+            Hello{" "}
+            <span className="textGradient text-[75px]">
+              {currentUser?.displayName
+                ? currentUser.displayName
+                : currentUser?.email}
+            </span>
+            , you are now logged in.
+          </h3>
+        </div>
+
+        {/* BUTTONS */}
+        <div className="w-[50%] flex justify-center">
+          {userLoggedIn ? (
+            <>
+              <button
+                onClick={() => {
+                  doSignOut().then(() => navigate("/login"));
+                }}
+                className="p-5 rounded-full cursor-pointer text-[16px] btnGradient w-[50%]"
+                type="button"
+              >
+                Logout
+              </button>
+            </>
+          ) : (
+            <div className="flex gap-2 w-full">
+              <Link
+                className="w-full p-3 rounded-full cursor-pointer text-[16px] btnGradient text-center"
+                to={"/login"}
+              >
+                Login
+              </Link>
+
+              <Link
+                className="w-full p-3 rounded-full cursor-pointer text-[16px] btnGradient text-center"
+                to={"/register"}
+              >
+                Register New Account
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
--- src/components/verificationEmailMessage/verificationEmailMessage.jsx
+++ src/components/verificationEmailMessage/verificationEmailMessage.jsx
@@ -1,10 +1,38 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useAuth } from "../../contexts/authContext";
+
 const VerificationEmailMessage = () => {
   const { currentUser } = useAuth();
 
-  return <section>VerificationEmailMessage</section>;
+  return (
+    <section className="section h-screen flex items-center justify-center bg-Image p-5">
+      <div className="flex flex-col text-center items-center gap-20">
+
+        {/* HEADER */}
+        <h1 className="textGradient text-[40px] md:text-[130px] leading-none">
+          Check Your Email
+        </h1>
+
+        {/* MESSAGE */}
+        <p className="text-[30px] md:text-[60px] text-white w-[90%] md:w-[90%] leading-tight">
+          We've sent a verification link to your email{" "}
+          <span className="font-bold">
+            {currentUser?.displayName
+              ? currentUser.displayName
+              : currentUser?.email}
+          </span>
+        </p>
+
+        {/* BUTTON */}
+        <button className="btnGradient w-[300px] md:w-[500px] md:p-5 cursor-pointer text-[20px] rounded-full">
+          <Link to={"/login"}>Continue Log In</Link>
+        </button>
+
+        <div className="bg-white h-[1px] w-[90%] opacity-40"></div>
+      </div>
+    </section>
+  );
 };
 
 export default VerificationEmailMessage;
